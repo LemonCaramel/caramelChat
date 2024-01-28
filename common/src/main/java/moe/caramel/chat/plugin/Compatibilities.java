06597ea9d86a83435bebbbaa3419f8bf0a3984fe
LemonCaramel <admin@caramel.moe>
2024-01-29 02:20:54 +0900

    Add Mixin filter for improving compatibility with other mods

---
+++ common/src/main/java/moe/caramel/chat/plugin/Compatibilities.java
@@ -0,0 +1,51 @@
+package moe.caramel.chat.plugin;
+
+import org.jetbrains.annotations.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Compatibility data registry
+ */
+public final class Compatibilities {
+
+    private static final Map<String, Data> REGISTRY = new HashMap<>();
+
+    // ================================
+
+    /**
+     * Gets the compatibility data.
+     *
+     * @param mixinClass Mixin class name
+     * @return Compatibility data
+     */
+    @Nullable
+    public static Data getData(final String mixinClass) {
+        return Compatibilities.REGISTRY.get(mixinClass);
+    }
+
+    /**
+     * Compatibility data.
+     *
+     * @param name Mod name
+     * @param targetClassName Detect target class name
+     * @param classes Mixin classes
+     */
+    public record Data(String name, String targetClassName, Set<String> classes) {}
+
+    /**
+     * Register compatibility data.
+     *
+     * @param name Mod name
+     * @param targetClassName Detect target class name
+     * @param classes Mixin classes
+     */
+    private static Data register(final String name, final String targetClassName, final Set<String> classes) {
+        final Data data = new Data(name, targetClassName, classes);
+        for (final String clazz : classes) {
+            Compatibilities.REGISTRY.put(clazz, data);
+        }
+        return data;
+    }
+}
